3d8e729e3f26cb9184612c9a47e18bbd24e7234c
Alexander Harris <aph1102@jagmail.southalabama.edu>
2022-10-04 12:57:02 -0500

    first commit-- Navbar completed

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCxp359D3ezHyNoS8_9VpIzHEUmHWHQwjc",
+  authDomain: "netflix-clone-11be8.firebaseapp.com",
+  projectId: "netflix-clone-11be8",
+  storageBucket: "netflix-clone-11be8.appspot.com",
+  messagingSenderId: "382985367273",
+  appId: "1:382985367273:web:5837b0640c90610ece8a78",
+};
